0ad8c071017d3b1945f21d3b88009bd01134af99
JannikeL <jannike@rymden.nu>
2016-03-14 16:51:42 +0100

    Added files via upload
    Garadingtest: rättning av prov med databaskoppling

--- src/gui/GradingTest.java
+++ src/gui/GradingTest.java
@@ -1,11 +1,20 @@
 package gui;
 
 import java.util.ArrayList;
+import java.util.List;
+
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.Persistence;
+import javax.persistence.Query;
 
 import com.sun.glass.ui.GestureSupport;
 
 import entity.Question;
+import entity.SchoolClass;
 import entity.StudentAnswer;
+import entity.Test;
+import entity.User;
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableFloatArray;
 import javafx.collections.ObservableList;
@@ -16,6 +25,8 @@ import javafx.scene.control.Alert;
 import javafx.scene.control.Button;
 import javafx.scene.control.ComboBox;
 import javafx.scene.control.Label;
+import javafx.scene.control.Menu;
+import javafx.scene.control.MenuItem;
 import javafx.scene.control.PasswordField;
 import javafx.scene.control.TextArea;
 import javafx.scene.control.TextField;
@@ -28,14 +39,24 @@ import javafx.scene.text.Font;
 import javafx.scene.text.Text;
 
 public class GradingTest {
-	
+
 	private String whiteBGAndGreyBorder = "-fx-background-color: #FFFFFF; " + "-fx-border-color: #D3D3D3";
 
 	private String frameStyle = "-fx-border-color: #FFA500; " + "-fx-border-width: 2px; "
 			+ "-fx-background-color: #FFFFFF";
+	private EntityManagerFactory emf;
+	private EntityManager em;
+	private Menu gradingTestMenu;
+	private Menu mnuFile;
 
-	
+	public GradingTest(Menu mnuFile) {
+		this.mnuFile = mnuFile;
+
+	}
+
 	public StackPane showGradingPane() {
+		emf = Persistence.createEntityManagerFactory("Testverktyg");
+		em = emf.createEntityManager();
 		ObservableList<Double> givenPointAlternative = FXCollections.observableArrayList();
 		givenPointAlternative.add(-1.0);
 		givenPointAlternative.add(-0.5);
@@ -50,125 +71,172 @@ public class GradingTest {
 		givenPointAlternative.add(4.0);
 		givenPointAlternative.add(4.5);
 		givenPointAlternative.add(5.0);
-		
+
 		StackPane gradingPane = new StackPane();
-		
-		
-		Text titleText = new Text("Grading test");
-		titleText.setFont(Font.font(30));
-		HBox titleBox = new HBox(10);
-		titleBox.setPadding(new Insets(10, 10, 5, 10));
-		titleBox.setAlignment(Pos.CENTER);
-		titleBox.getChildren().add(titleText);
 
-		// PSEUDOKOD TODO
-		// Studentens namn
-		Label lblName = new Label("Studentens namn"); // studentens namn
-		//Label lblName = new Label(StudentAnswer.User.getfName() + " " + StudentAnswer.User.getlName()); // studentens namn
-		lblName.setMaxWidth(200);
-		
-		// provets namn
-		Label lblTestName = new Label("Provets namn"); 
-//		Label lblTestName = new Label(Question.Test.getTestName()); 
-		lblTestName.setMaxWidth(200);
+		gradingTestMenu = new Menu("Available tests");
+		mnuFile.getItems().add(gradingTestMenu);
+		Query findTests = em.createQuery("Select t from Test t");
+		List<Test> testList = findTests.getResultList();
 
-		// frågans nummer/namn
-		Label lblQuestionName = new Label("Frågans titel");
-//		Label lblQuestionName = new Label(Question.getQuestionTitle());
-		lblQuestionName.setMaxWidth(200);
+		// TODO
+		Test t2 = new Test("test 2", "20160317-09:00", "20160317-11:00");
+		testList.add(t2);
+		// TODO
 
-		// frågetexten, textarea bästa alternativet?
-		TextArea txtQuestion = new TextArea("Frågetexten");
-//		TextArea txtQuestion = new TextArea(Question.getQuestionText());
-		txtQuestion.setDisable(true);
-		txtQuestion.setMaxWidth(200);
+		for (Test t : testList) {
+			Menu itemForTestMenu = new Menu("Test: " + t.getTestName() + " " + "date: " + t.getTestStart());
+			gradingTestMenu.getItems().add(itemForTestMenu);
+			for (SchoolClass sc : t.getClasses()) {
+				Menu itemForClassMenu = new Menu("Class: " + sc.getClassName());
+				itemForTestMenu.getItems().add(itemForClassMenu);
+				for (User u : sc.getStudents()) {
+					System.out.println(u.getUserName());
+					MenuItem itemForStudentMenu = new MenuItem(u.getfName() + " " + u.getlName());
+					itemForClassMenu.getItems().add(itemForStudentMenu);
+					itemForClassMenu.setOnAction(event -> {
+						Text titleText = new Text("Grading test");
+						titleText.setFont(Font.font(30));
+						HBox titleBox = new HBox(10);
+						titleBox.setPadding(new Insets(10, 10, 5, 10));
+						titleBox.setAlignment(Pos.CENTER);
+						titleBox.getChildren().add(titleText);
 
-		// rubrik, "studentens svar"
-		Label lblAnswerTitle = new Label("Student's answer");
-		lblAnswerTitle.setMaxWidth(200);
+						// Studentens namn
+						Label lblName = new Label(u.getfName() + " " + u.getlName()); // studentens
+																						// namn
+						lblName.setFont(Font.font(18));
+						lblName.setMaxWidth(200);
 
-		// studentens svar
-		TextArea txtStudentAnswer = new TextArea("Studentens svar");
-//		TextField txtCity = new TextField(StudentAnswer.getAnswer());
-		txtStudentAnswer.setDisable(true);
-		txtStudentAnswer.setMaxWidth(200);
+						// provets namn
+						Label lblTestName = new Label(t.getTestName());
+						lblTestName.setFont(Font.font(18));
+						lblTestName.setMaxWidth(200);
 
-		// rubrik: lärarens egna noteringar
-		Label lblTeachersNote = new Label("Teacher's note");
-		lblTeachersNote.setMaxWidth(200);
+						for (Question q : t.getQuestions()) {
+							// frågans nummer/namn
+							Label lblQuestionName = new Label(q.getQuestionTitle());
+							lblQuestionName.setMaxWidth(200);
 
-		// lärarens egna noteringar
-		TextArea txtTeachersNote = new TextArea();
-		txtTeachersNote.setMaxWidth(200);
+							// frågetexten, textarea bästa alternativet?
+							TextArea txtQuestion = new TextArea(q.getQuestionText());
+							txtQuestion.setDisable(true);
+							txtQuestion.setMaxWidth(200);
+							txtQuestion.setMaxHeight(100);
 
-		// rubrik: lärarens kommentarer till studenten
-		Label lblTeachersComment = new Label("Teacher's comment to the student");
-		lblTeachersComment.setMaxWidth(200);
+							// rubrik, "studentens svar"
+							Label lblAnswerTitle = new Label("Student's answer");
+							lblAnswerTitle.setMaxWidth(200);
 
-		// lärarens kommentarer till studenten
-		TextArea txtTeachersComment = new TextArea();
-		txtTeachersComment.setMaxWidth(200);
+							// studentens svar
+							TextArea txtStudentAnswer = new TextArea();
+							Query answerQuery = em.createNamedQuery("getStudentAnswer");
+							answerQuery.setParameter("question", q);
+							answerQuery.setParameter("user", u);
+							StudentAnswer sa = (StudentAnswer) answerQuery.getSingleResult();
+							txtStudentAnswer.setDisable(true);
+							txtStudentAnswer.setMaxWidth(200);
+							txtStudentAnswer.setMaxHeight(100);
+							txtStudentAnswer.setText(sa.getAnswer());
 
-		// rubrik: sätta poäng
-		Label lblGivenPoints = new Label("Set points");
-		lblGivenPoints.setMaxWidth(200);
+							// rubrik: lärarens egna noteringar
+							Label lblTeachersNote = new Label("Teacher's note");
+							lblTeachersNote.setMaxWidth(200);
 
-		// poäng på uppgiften
-		ComboBox cmbGivenPoints = new ComboBox();
-		cmbGivenPoints.setItems(givenPointAlternative);
-		cmbGivenPoints.setValue(0.0);
-		cmbGivenPoints.setStyle(whiteBGAndGreyBorder);
-		cmbGivenPoints.setCursor(Cursor.HAND);
-		cmbGivenPoints.setPromptText("Set points");
+							// lärarens egna noteringar
+							TextArea txtTeachersNote = new TextArea();
+							if (sa.getNote() != null) {
+								txtTeachersNote.setText(sa.getNote());
+							}
+							txtTeachersNote.setMaxWidth(200);
 
-		VBox gradingBoxL = new VBox(5);
-		gradingBoxL.setPadding(new Insets(5, 10, 5, 10));
-		gradingBoxL.getChildren().addAll(lblName, lblQuestionName, txtQuestion, 
-				lblTeachersNote, txtTeachersNote);
+							// rubrik: lärarens kommentarer till studenten
+							Label lblTeachersComment = new Label("Teacher's comment to the student");
+							lblTeachersComment.setMaxWidth(200);
 
-		VBox gradingBoxR = new VBox(5);
-		gradingBoxR.setPadding(new Insets(5, 10, 5, 10));
-		gradingBoxR.getChildren().addAll(lblTestName, lblAnswerTitle, 
-				txtStudentAnswer, lblTeachersComment, txtTeachersComment, 
-				lblGivenPoints, cmbGivenPoints);
+							// lärarens kommentarer till studenten
+							TextArea txtTeachersComment = new TextArea();
+							if (sa.getTeacherComment() != null) {
+								txtTeachersComment.setText(sa.getTeacherComment());
+							}
+							txtTeachersComment.setMaxWidth(200);
 
-		HBox gradingBoxes = new HBox();
-		gradingBoxes.getChildren().addAll(gradingBoxL, gradingBoxR);
+							// rubrik: sätta poäng
+							Label lblGivenPoints = new Label("Set points");
+							lblGivenPoints.setMaxWidth(200);
 
-		Button saveButton = new Button("Save");
-		Button cancelButton = new Button("Cancel");
-		saveButton.setStyle(whiteBGAndGreyBorder);
-		BorderPane buttons = new BorderPane();
-		saveButton.requestFocus();
-		saveButton.setCursor(Cursor.HAND);
-		cancelButton.setStyle(whiteBGAndGreyBorder);
-		cancelButton.setCursor(Cursor.HAND);
+							// poäng på uppgiften
+							ComboBox<Double> cmbGivenPoints = new ComboBox();
+							cmbGivenPoints.setItems(givenPointAlternative);
 
-		buttons.setLeft(saveButton);
-		buttons.setRight(cancelButton);
-		buttons.setPadding(new Insets(5, 10, 10, 10));
+							if (sa.getGivenPoints() != null) {
+								cmbGivenPoints.setValue(sa.getGivenPoints());
+							} else {
+								cmbGivenPoints.setPromptText("Set points");
+							}
+							cmbGivenPoints.setStyle(whiteBGAndGreyBorder);
+							cmbGivenPoints.setCursor(Cursor.HAND);
 
-		cancelButton.setOnAction(cancel -> {
-			Alert notSaved = new Alert(AlertType.ERROR);
-// varning för att innehållet inte är sparat
+							VBox gradingBoxL = new VBox(5);
+							gradingBoxL.setPadding(new Insets(5, 10, 5, 10));
+							gradingBoxL.getChildren().addAll(lblName, lblQuestionName, txtQuestion, lblTeachersNote,
+									txtTeachersNote);
 
-			
-			
-		});
+							VBox gradingBoxR = new VBox(5);
+							gradingBoxR.setPadding(new Insets(5, 10, 5, 10));
+							gradingBoxR.getChildren().addAll(lblTestName, lblAnswerTitle, txtStudentAnswer,
+									lblTeachersComment, txtTeachersComment, lblGivenPoints, cmbGivenPoints);
 
-		
-		String regex = "\\d+";
+							HBox gradingBoxes = new HBox();
+							gradingBoxes.getChildren().addAll(gradingBoxL, gradingBoxR);
 
-		saveButton.setOnAction(save -> {
-			// spara note, comment, points
-		});
+							Button saveButton = new Button("Save");
+							Button cancelButton = new Button("Cancel");
+							saveButton.setStyle(whiteBGAndGreyBorder);
+							BorderPane buttons = new BorderPane();
+							saveButton.requestFocus();
+							saveButton.setCursor(Cursor.HAND);
+							cancelButton.setStyle(whiteBGAndGreyBorder);
+							cancelButton.setCursor(Cursor.HAND);
 
-		VBox addUserColumn = new VBox(5);
-		addUserColumn.setPadding(new Insets(0));
-		addUserColumn.getChildren().addAll(titleBox, gradingBoxes, buttons);
-		addUserColumn.setStyle(frameStyle);// sets style for addframe
-		gradingPane.getChildren().add(addUserColumn);
+							buttons.setLeft(saveButton);
+							buttons.setRight(cancelButton);
+							buttons.setPadding(new Insets(5, 10, 10, 10));
+
+							cancelButton.setOnAction(cancel -> {
+								Alert notSaved = new Alert(AlertType.ERROR);
+								// varning för att innehållet inte är sparat
+
+							});
+
+							saveButton.setOnAction(save -> {
+
+								sa.setGivenPoints(cmbGivenPoints.getValue());
+								sa.setNote(txtTeachersNote.getText());
+								sa.setTeacherComment(txtTeachersComment.getText());
+								em.getTransaction().begin();
+								em.persist(sa);
+								em.getTransaction().commit();
+							});
+
+							HBox separator = new HBox(5);
+							separator.setPadding(new Insets(5));
+							separator.setStyle("-fx-background-color: #FFA500");
+							VBox addUserColumn = new VBox(5);
+							addUserColumn.setPadding(new Insets(0));
+							addUserColumn.getChildren().addAll(titleBox, gradingBoxes, buttons, separator);
+							addUserColumn.setStyle("-fx-background-color: #FFFFFF");// TODO
+																					// sets
+																					// style
+																					// for
+																					// addframe
+
+							gradingPane.getChildren().add(addUserColumn);
+						}
+					});
+				}
+			}
+		}
 		return gradingPane;
 	}
-
 }
