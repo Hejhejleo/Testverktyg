ce643a6017587f3aa278ddb725ccdc8a66a860c3
Leonard Asp <Leoasp96@gmail.com>
2016-03-17 14:08:56 +0100

    Update Question.java

--- src/entity/Question.java
+++ src/entity/Question.java
@@ -1,6 +1,7 @@
 package entity;
 
 import javax.persistence.CascadeType;
+import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
